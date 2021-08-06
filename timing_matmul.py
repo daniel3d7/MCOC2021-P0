8e254272e49fde385f71221632345b50982f07c5
daniel3d7 <88356329+daniel3d7@users.noreply.github.com>
2021-08-06 12:32:20 -0400

    Update timing_matmul.py

--- timing_matmul.py
+++ timing_matmul.py
@@ -1,22 +1,52 @@
-from numpy import zeros
+from scipy import matmul, rand
 from time import perf_counter
+from numpy import zeros , float16,float32,float64
+import matplotlib.pylab as plt
 
-
-
-#Tamaño
 N = 1000
-A = zeros((N, N))+1
-B = zeros((N, N))+2
+Ns = [1, 2, 3, 5, 7, 10, 15, 21, 30, 42, 60, 84, 119, 167, 236, 332, 467, 657, 925, 1301, 1831, 2577, 3626, 5102, 7179]
 
 
-t1 = perf_counter()
-C = A@B
-t2 = perf_counter()
+dts = []
+
+mems = []
+
+fid = open("rendimiento.txt", "w")
+
+contador = 0
+
+while contador < 10 :
+    
+    for N in Ns:
+    
+
+        A = zeros((N,N), dtype = float32() ) + 1
+        uso_memoria_teorico = 4*N*N
+        uso_memoria_numpy = A.nbytes
+    
+ 
+    
+   
+        B = zeros((N,N)) + 2
 
 
-dt = t2 - t1
+        t1 = perf_counter()
 
-print(f"dt = {dt} s")
+        C = A@B
 
-print ("Utilizando la funcion matmul:") 
-print ("AB =", np.matmul(A,B))
+        t2 = perf_counter()
+    
+        uso_memoria_total = A.nbytes + B.nbytes + C.nbytes
+        dt = t2 - t1
+        dts.append(dt)
+    
+        mems.append(uso_memoria_total)
+    
+        print (f"N = {N}  dt = {dt} s")
+    
+        fid.write(f"{N}  {dt}  {uso_memoria_total} \n")
+    contador += 1
+
+fid.close()
+
+
